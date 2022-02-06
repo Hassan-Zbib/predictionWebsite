0d030dbf344ab960b6e480834a820181b50e542a
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-02-06 15:35:36 +0200

    added fetch functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,32 @@
 const getDogImg = async () => {
-    const url = 'https://dog.ceo/api/breeds/image/random'
+    const url = new URL('https://dog.ceo/api/breeds/image/random')
     const response = await fetch(url).then(res => res.json())
     return response.message
 }
 
 const getPredictionData = async name => {
-    const age_pred = await fetch(`https://api.agify.io/?name=${name}`).then(res => res.json())
-    const gndr_pred = await fetch(`https://api.genderize.io?name=${name}`).then(res => res.json())
-    const nat_pred = await fetch(`https://api.nationalize.io/?name=${name}`).then(res => res.json())
+    const [age_pred, gndr_pred, nat_pred] = await Promise.all([
+        fetch(`https://api.agify.io/?name=${name}`).then(res => res.json()),
+        fetch(`https://api.genderize.io?name=${name}`).then(res => res.json()),
+        fetch(`https://api.nationalize.io/?name=${name}`).then(res => res.json())
+    ])
 
-
+    return {
+        age: age_pred.age,
+        gender: gndr_pred.gender,
+        countries: nat_pred.country
+    }
 
 }
 
+const main = () => {
+    getDogImg()
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+
+    getPredictionData('joe')
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+}
+
+window.onload = main
